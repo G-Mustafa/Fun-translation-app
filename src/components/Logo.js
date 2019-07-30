ea4653547bf391c331c7bfc1684ec165f4c83658
G-Mustafa <ghulammustafa137@gmail.com>
2019-07-30 17:55:50 +0500

    final

--- src/components/Logo.js
+++ src/components/Logo.js
@@ -2,7 +2,7 @@ import React from 'react';
 import Tilt from 'react-tilt';
 
 const Logo = () => (
-    <Tilt className="Tilt" options={{ max : 35,scale : 1.1 }} style={{ height: 250, width: 250 }} >
+    <Tilt className="Tilt" options={{ max : 35,scale : 1.1 }} style={{ height: 150, width: 150, margin: 20 }} >
         <div id='logo' className="Tilt-inner"></div>
     </Tilt>
     )
